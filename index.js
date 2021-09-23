3ca53e787d00a525787269da9cb3624872b31fb5
Adam <adammoore118@outlook.com>
2021-09-23 09:02:51 -0400

    commit tree 09ef687a11e9c3a30a55e61e5afc36e0617b413f

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+
+function findMatching(drivers){
+    return drivers.filter(driver => driver.toLowerCase>-1)
+}
+
+// function findMatching(drivers, name){
+
+//     return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+  
+//   }
+
